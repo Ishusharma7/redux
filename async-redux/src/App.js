1e7f2343c25631d82897508d2a0089a47f744a0f
“[Ishusharma7]” <sharmaishu12356@gmail.com>
2023-07-28 19:04:07 +0530

    third commit

---
+++ async-redux/src/App.js
@@ -0,0 +1,15 @@
+import CakeContainer from './Components/CakeContainer';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+function App() {
+  return (
+    <Provider store = {store}>
+    <div className="App">
+     <CakeContainer />
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ async-redux/src/Cake/cakeReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    numberofcakes: 10
+}
+const cakeReducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE:
+            return{
+                ...state, 
+                numberofcakes:state.numberofcakes -1
+            }
+            default: return state
+    }
+}
+export default cakeReducer
